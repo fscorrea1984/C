2f5deaa0896417a01742ae98fc0a511ff84b4352
Frederico Correa <fscorrea1984@hotmail.com>
2023-01-15 07:52:05 -0300

    2023-01-15

---
+++ Elementary Data Structures/Linked Lists/Doubly Linked List/doublylinkedlist.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  int key;
+  Node *next;  
+  Node *prev;
+} Node;
+
+void insert(Node *L,int k) {
+  Node *aux = (Node *)malloc(sizeof(Node));
+  aux->key = k;
+  aux->next = L->next;
+  aux->prev = L;
+  L->next = aux;
+}
+
+void delete(Node *L) {
+  Node *aux = L;
+  L = L->next;
+  L->prev = aux->prev;
+  L->prev->next = L;
+  aux->next = NULL;
+  aux->prev = NULL;
+  free(aux);
+}
+
+Node *search(Node *L,int k) {
+  Node *aux = L;
+  while(aux != NULL) {
+    if(aux->key == k)
+      return(aux);
+    else
+      aux = aux->next;
+  }
+  return(aux);
+}
+
+void printlist(Node *L) {
+  if(L == NULL) {
+    puts("Empty list");
+    goto END;
+  }
+  Node *aux = L;
+  while(aux != NULL) {
+    if(aux->next != NULL)
+      printf("%d, ",aux->key);
+    else
+      printf("%d\n",aux->key);
+  }
+ END:
+}
+
+int main(int argc, char **argv) {
+
+  /* --- */
+
+  // dynamically allocate first node of list
+  Node *L = (Node *)malloc(sizeof(Node));
+  L->key = 4;
+  L->next = NULL;
+  L->prev = NULL;
+
+  insert(L,7);
+  insert(L,13);
+
+  printlist(L);
+
+  Node *N;
+  
+  if((N = search(N,20)) == NULL)
+    puts("Value not found");
+  else
+    delete(N);
+
+  if((N = search(N,7)) == NULL)
+    puts("Value not found");
+  else
+    delete(N);
+
+  printlist(L);
+
+  // free allocated nodes
+  Node *aux = L;
+  while(aux != NULL) {
+    Node *N = aux;
+    aux = aux->next;
+    free(N);
+  }
+  */
+  
+  return 0;
+
+}
